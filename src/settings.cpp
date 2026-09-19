5a5b6d577b848cf370c24fc94142df1dab66ee88
priyanshusharmapc <priyanshusharma.pc@gmail.com>
2026-09-19 21:51:17 +0530

    fix(flatpak): join temp cleanup path safely [MDPS-AUDIT2-174]

--- src/settings.cpp
+++ src/settings.cpp
@@ -1306,7 +1306,7 @@ void settings::clearFlatPakTemps()
 {
 	if( utility::platformisFlatPak() ){
 
-		auto ee = m_appDataPath + "tmp" ;
+		auto ee = QDir( m_appDataPath ).filePath( "tmp" ) ;
 
 		directoryManager::readAll( ee ).forEachFile( [ & ]( const QString& e ){
 
