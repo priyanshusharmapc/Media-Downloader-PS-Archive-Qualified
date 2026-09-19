a892bfd7d3432cfea4c85465b16930e17c2a0157
priyanshusharmapc <priyanshusharma.pc@gmail.com>
2026-09-19 21:38:49 +0530

    fix(files): open local folders with file URLs [MDPS-AUDIT2-156]

--- src/utility.cpp
+++ src/utility.cpp
@@ -1281,7 +1281,7 @@ void utility::openDownloadFolderPath( const QString& url )
 
 		QProcess::startDetached( "explorer.exe",{ QDir::toNativeSeparators( url ) } ) ;
 	}else{
-		QDesktopServices::openUrl( url ) ;
+		QDesktopServices::openUrl( QUrl::fromLocalFile( url ) ) ;
 	}
 }
 
