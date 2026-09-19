e529db75eeec70bce79cb18afeac90307a80eb48
priyanshusharmapc <priyanshusharma.pc@gmail.com>
2026-09-19 22:30:39 +0530

    fix(menu): bind engine override to captured row [MDPS-AUDIT2-159]

--- src/playlistdownloader.cpp
+++ src/playlistdownloader.cpp
@@ -665,11 +665,15 @@ void playlistdownloader::customContextMenuRequested()
 		}
 	}
 
-	connect( mm,&QMenu::triggered,[ this ]( QAction * ac ){
+	connect( mm,&QMenu::triggered,[ this,row ]( QAction * ac ){
+
+		if( row < 0 || row >= m_table.rowCount() ){
+			return ;
+		}
 
 		auto u = tableWidget::type::EngineName ;
 
-		m_table.setDownloadingOptions( u,m_table.currentRow(),ac->objectName() ) ;
+		m_table.setDownloadingOptions( u,row,ac->objectName() ) ;
 	} ) ;
 
 	auto subMenu = utility::setUpMenu( m_ctx,{},false,false,true,&m ) ;
