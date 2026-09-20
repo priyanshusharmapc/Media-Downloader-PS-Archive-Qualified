2b40809bdc6d21f8ef0ea582e07e037758c9aedd
priyanshusharmapc <priyanshusharma.pc@gmail.com>
2026-09-20 05:38:33 +0530

    fix(save-list): classify JSON by semantic suffix [AUDIT2-113]

--- src/utility.cpp
+++ src/utility.cpp
@@ -1547,7 +1547,7 @@ void utility::saveDownloadList( const Context& ctx,QMenu& m,tableWidget& tableWi
 
 			const auto e = _saveDownloadList( tableWidget,false ) ;
 
-			if( s.endsWith( ".json",Qt::CaseInsensitive ) ){
+			if( QFileInfo( s ).suffix().compare( "json",Qt::CaseInsensitive ) == 0 ){
 
 				auto m = QJsonDocument( e ).toJson( QJsonDocument::Indented ) ;
 
